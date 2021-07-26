dc13b2798efdae4e2ac544a8de04838385926342
seungho <50465901+csh9066@users.noreply.github.com>
2021-07-26 21:55:32 +0900

    first commit

---
+++ src/main/java/com/leaf/shop/LeafShopApplication.java
@@ -0,0 +1,13 @@
+package com.leaf.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafShopApplication.class, args);
+    }
+
+}
